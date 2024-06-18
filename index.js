5213138ee2ce2e85dd965d8944f2afae7f0a2fbd
Jibitesh-Chakraborty2811 <85732128+Jibitesh-Chakraborty2811@users.noreply.github.com>
2024-06-18 09:29:14 +0530

    new change

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ app.get('/',(req,res)=>{
     res.json({message:'Hello World'});
 })
 
+app.get('/show',(req,res)=>{
+    res.json({message:'This is show path'})
+})
+
 app.listen(9000,()=>{
     console.log('Port Started on port 9000');
 })
